4869986e5b39c3136e451f382b54237288abe9d7
Muhammad Ahsan <180093008+Muhammad-Farhan1@users.noreply.github.com>
2025-07-15 12:03:49 +0500

    feat: implement script for deleting columns from DataFrame

---
+++ deleteColumns.py
@@ -0,0 +1,18 @@
+import pandas as pd
+user_info = {
+    "names": ["Ali", "Sara", "John", "Fatima", "Omar", "Ayesha", "David", "Zara"],
+    "ages": [25, 30, 22, 28, 35, 27, 24, 29],
+    "cities": ["Lahore", "Karachi", "Islamabad", "Rawalpindi", "Multan", "Peshawar", "Quetta", "Faisalabad"],
+    "scores": [88, 92, 75, 85, 90, 78, 95, 80]
+}
+
+data = pd.DataFrame(user_info)
+#print(data)
+
+#Deleting single columns:
+data.drop(columns="cities", inplace=True)
+print(data)
+
+#Deleting multiple columns:
+data.drop(columns=["scores","ages"] , inplace=True)
+print(data)
